ba5cce58e233206aef0cf478496cf2556816e395
Om Patel <ompatel@Oms-MacBook-Pro.local>
2023-08-15 17:44:09 -0400

    Inital commit

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import ejs from "ejs";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const port = 3000; 
+const app = express(); 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+const userSchema = new mongoose.Schema ({
+    password: String, 
+    email: String
+});
+
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", (req, res)=>{
+    res.render("home.ejs");
+});
+
+
+app.listen(port, function(){
+    console.log("Server started on port 3000.");
+});
---
+++ public/script.js
@@ -0,0 +1,40 @@
+// Linked to the footer.ejs to provide functionality to application
+
+
+// Manual hover functions, changing the button background color and text color. 
+
+$("#loginButton").on( "mouseenter", 
+function(){
+    $("#loginButton").removeClass("addSemiDarkColor");
+    $("#loginButton").addClass("addBlueColor");
+    $("#loginButton").css("color", "white");
+}).on( "mouseleave",  
+function(){
+    $("#loginButton").css("color", "black");
+    $("#loginButton").removeClass("addBlueColor"); 
+    $("#loginButton").addClass("addSemiDarkColor");
+});
+
+
+$("#signUpButton").on( "mouseenter", 
+function(){
+    $("#signUpButton").css("color", "black");
+    $("#signUpButton").removeClass("addDarkColor");
+    $("#signUpButton").addClass("addBlueColor");
+}).on( "mouseleave",  
+function(){
+    $("#signUpButton").css("color", "white");
+    $("#signUpButton").removeClass("addBlueColor"); 
+    $("#signUpButton").addClass("addDarkColor");
+});
+
+$("#headerText").on( "mouseenter", 
+function(){
+    $("#headerText").text("Om Patel");
+    $("#headerText").css("font-size", "20px");
+    $(this).attr("href", "https://www.linkedin.com/in/om-patel-726b19243/"); 
+}).on( "mouseleave",  
+function(){
+    $("#headerText").text("The Vault");
+    $("#headerText").css("font-size", "20px");
+});
